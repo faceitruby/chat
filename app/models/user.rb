e96ba321fef10f709e1a52be542c63f6346ba666
magas4891 <magasishe@gmail.com>
2020-01-21 12:43:25 +0200

    user validation of fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,14 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable,
-    :registerable,
-    :recoverable,
-    :rememberable,
-    :validatable,
-    :confirmable
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :validatable,
+         :confirmable
+
+  validates :first_name, :last_name, :email, presence: true, on: :create
+  validates :email, uniqueness: true
+  validates :password, length: { minimum: 6 }
+  validates :password, presence: true
 end
