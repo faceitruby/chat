bba5116dded29054440086538c8f389f04c64879
magas4891 <magasishe@gmail.com>
2020-01-21 12:43:25 +0200

    user validation of fields

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,13 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-    context 'Have fields' do
-      %i[
-        id created_at updated_at email encrypted_password reset_password_token
-        remember_created_at reset_password_sent_at unconfirmed_email
-        last_name first_name confirmation_token confirmed_at confirmation_sent_at
-      ].each do |field|
-        it { is_expected.to have_db_column(field) }
-      end
+  context 'Have fields' do
+    %i[
+      id created_at updated_at email encrypted_password reset_password_token
+      remember_created_at reset_password_sent_at unconfirmed_email
+      last_name first_name confirmation_token confirmed_at confirmation_sent_at
+    ].each do |field|
+      it { is_expected.to have_db_column(field) }
+    end
+  end
+
+  context 'validation' do
+    %i[first_name
+       last_name
+       email
+       password].each do |field|
+      it { is_expected.to validate_presence_of(:"#{field}") }
+    end
   end
 end
