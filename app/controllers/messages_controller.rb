28afca778e5a9275ca30158e0493d5131f8fe0a6
Vladimir <timos9vs@gmail.com>
2020-01-22 16:20:11 +0200

    skeleton for messages controllers

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,6 @@
 class MessagesController < ApplicationController
   def create
-    
+    chat_member = ChatMember.where(user: current_user, chat_room: ChatRoom.find(params[:chat_room])).first
+    chat_member.messages.create!(message: params[:message])
   end
 end
