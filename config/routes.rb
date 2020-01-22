6c60057a05d0b747bcd4122d920978448e3f54b4
Vladimir <timos9vs@gmail.com>
2020-01-22 17:24:13 +0200

    add create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  get 'chat_rooms/create'
-  get 'chat_members/create'
-  get 'messages/create'
   get 'home_pages/index'
   devise_for :users
   get 'persons/profile'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'home_pages#index'
+
+  resources :chat_members, only: %i[create new]
+  resources :chat_rooms, only: %i[create new]
+  resources :messages, only: %i[create new]
 end
