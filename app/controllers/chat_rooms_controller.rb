129877823fee88fb951c5c88253651e06c211d53
Vladimir <timos9vs@gmail.com>
2020-01-23 17:02:15 +0200

    chat room show controller

--- app/controllers/chat_rooms_controller.rb
+++ app/controllers/chat_rooms_controller.rb
@@ -1,4 +1,13 @@
 class ChatRoomsController < ApplicationController
+
+  def show
+    @new_chat_room = ChatRoom.new
+    @chat_list = ChatMember.where(user: current_user).map { |chat_member| chat_member.chat_room }
+    chat_room = ChatRoom.find(params[:id])
+    @chat_members = ChatMember.where(chat_room: chat_room)
+    @chat_messages = @chat_members.find {|chat_member| Message.where(chat_member: chat_member) }
+  end
+
   def new
     @chat_room = ChatRoom.new
   end
