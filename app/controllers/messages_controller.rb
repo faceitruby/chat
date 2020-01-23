5aa19fbed2640a779e04047e341b5be44445ed72
Vladimir <timos9vs@gmail.com>
2020-01-23 19:15:16 +0200

    Add create message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,7 @@
 class MessagesController < ApplicationController
   def create
-    chat_member = ChatMember.where(user: current_user, chat_room: ChatRoom.find(params[:chat_room])).first
+    chat_member = ChatMember.where(user: current_user, chat_room: ChatRoom.find(params[:chat_room_id])).first
     chat_member.messages.create!(message: params[:message])
+    redirect_back(fallback_location: root_path)
   end
 end
