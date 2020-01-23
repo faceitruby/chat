4a52a19ef31c282bc17e1d990035aa4717c204f1
Vladimir <timos9vs@gmail.com>
2020-01-23 19:15:16 +0200

    Add create message

--- app/controllers/chat_rooms_controller.rb
+++ app/controllers/chat_rooms_controller.rb
@@ -2,9 +2,11 @@ class ChatRoomsController < ApplicationController
 
   def show
     @new_chat_room = ChatRoom.new
+    @new_message = Message.new
+
     @chat_list = ChatMember.where(user: current_user).map { |chat_member| chat_member.chat_room }
-    chat_room = ChatRoom.find(params[:id])
-    @chat_members = ChatMember.where(chat_room: chat_room)
+    @chat_room = ChatRoom.find(params[:id])
+    @chat_members = ChatMember.where(chat_room: @chat_room)
     @chat_messages = @chat_members.find {|chat_member| Message.where(chat_member: chat_member) }
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'home_pages#index'
   resources :chat_members, only: %i[create new]
-  resources :chat_rooms, only: %i[create show]
-  resources :messages, only: %i[create new]
+  resources :chat_rooms, only: %i[create show] do
+    resources :messages, only: %i[create new]
+  end
 end
