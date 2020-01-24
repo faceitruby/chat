5069455642a193bf82968debddbf6e3affcffd16
Vladimir <timos9vs@gmail.com>
2020-01-24 08:00:46 +0200

    add creation message

--- app/controllers/chat_rooms_controller.rb
+++ app/controllers/chat_rooms_controller.rb
@@ -7,7 +7,8 @@ class ChatRoomsController < ApplicationController
     @chat_list = ChatMember.where(user: current_user).map { |chat_member| chat_member.chat_room }
     @chat_room = ChatRoom.find(params[:id])
     @chat_members = ChatMember.where(chat_room: @chat_room)
-    @chat_messages = @chat_members.find {|chat_member| Message.where(chat_member: chat_member) }
+    # @chat_messages = @chat_members.find { |chat_member| Message.where(chat_member: chat_member) }
+
   end
 
   def new
